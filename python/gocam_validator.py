d3532c80e828870c45138f1519097af8d23c4a6e
cmungall <cjm@berkeleybop.org>
2019-07-15 22:48:59 -0700

    Adding test suite and configuring travis to use python

--- python/gocam_validator.py
+++ python/gocam_validator.py
@@ -2,10 +2,11 @@ import click
 import logging
 from pyshex.evaluate import evaluate
 from rdflib import Graph, Namespace, URIRef
-from typing import Optional, Set, List, Union, Dict, Any
+from typing import Optional, Set, List, Union, Dict, Tuple, Any
 from rdflib.namespace import RDF, RDFS, OWL
 from SPARQLWrapper import SPARQLWrapper, JSON
 from cachier import cachier
+from dataclasses import dataclass, field
 import datetime
 
 
@@ -19,39 +20,70 @@ cmap = {
 
 shex_source = '../shapes/go-cam-shapes.shex'
 
-@click.command()
-@click.argument('rdf_file', nargs=-1)
-def validate(rdf_file : [str]):
+def get_shexc():
     with open(shex_source,'r') as s:
         shexc = s.read()
-        #print(f'Shex={shexc}')
-        
-    print(f'Files={rdf_file}')
+    return shexc
+
+@dataclass
+class ValidationReport():
+
+    shexc : Optional[str]
+    rdf_file: Optional[str]
+    all_successful : bool = True
+    nodes_with_no_shape : List[URIRef] = field(default_factory=lambda: [])
+    pass_list : Tuple[URIRef, URIRef, List[str]] = field(default_factory=lambda: [])
+    fail_list : Tuple[URIRef, URIRef, List[str]] = field(default_factory=lambda: [])
+    
+
+@click.command()
+@click.argument('rdf_file', nargs=-1)
+def validate_files(rdf_file : [str]):
+    shexc = get_shexc()
+    
     for f in rdf_file:
-        g = Graph()
-        g.parse(f, format="turtle")
-        inst_cls_tuples = []
-        smap = {}
-        for inst,p,cls in g.triples( (None, RDF.type, None) ):
-            inst_cls_tuples.append( (inst, cls) )
-            ancs = get_ancestors(cls)
-            #print(f'IC={inst} type {cls} ancs={len(ancs)}')
-            shape_classes = []
-            for a in ancs:
-                if a in cmap:
-                    # inject
-                    g.add( (cls, RDF.type, OWL.Class) )
-                    g.add( (cls, RDFS.subClassOf, URIRef(a)) )
-                    shape_classes.append(GOSHAPES[cmap[a]])
-            if shape_classes:
-                print(f'IC={inst} type {cls} sc={shape_classes} ancs={len(ancs)}')
-                smap[inst] = shape_classes
+        validate(f, shexc)
+        
+def validate(filename : str, shexc : Optional[str] = None) -> ValidationReport:
+    if shexc is None:
+        shexc = get_shexc()
+    g = Graph()
+    g.parse(filename, format="turtle")
+    inst_cls_tuples = []
+    smap = {}
+    rpt = ValidationReport(shexc=shexc, rdf_file=filename)
+
+    for inst,p,cls in g.triples( (None, RDF.type, None) ):
+        if cls in [OWL.Class, OWL.NamedIndividual, OWL.AnnotationProperty, OWL.ObjectProperty]:
+            continue
+        inst_cls_tuples.append( (inst, cls) )
+        ancs = get_ancestors(cls)
+        shape_classes = []
+        for a in ancs:
+            if a in cmap:
+                # inject
+                g.add( (cls, RDF.type, OWL.Class) )
+                g.add( (cls, RDFS.subClassOf, URIRef(a)) )
+                shape_classes.append(GOSHAPES[cmap[a]])
+        if shape_classes:
+            logging.debug(f'IC={inst} type {cls} sc={shape_classes} ancs={len(ancs)}')
+            smap[inst] = shape_classes
+        else:
+            rpt.nodes_with_no_shape.append(inst)
+            logging.warn(f"No shape class for tuple {inst} {cls}")
+
+    rpt.all_successful = True
+    for inst, shape_classes in smap.items():
+        for sc in shape_classes:
+            success, reason = evaluate(g, shexc, inst, sc)
+            if success:
+                logging.info(f"Success: {inst} {sc}")
+                rpt.pass_list.append( (inst, sc, reason) )
             else:
-                print(f"No shape class for tuple {inst} {cls}")
-        for inst, shape_classes in smap.items():
-            for sc in shape_classes:
-                rslt, reason = evaluate(g, shexc, inst, sc)
-                print(f"  Focus: {inst}\n SC: {sc}  Start: {rslt}\n  Reason: {str(reason)}")
+                all_successful = False
+                logging.info(f"Fail: {inst} {sc} Reason: {reason}")
+                rpt.fail_list.append( (inst, sc, reason) )
+    return rpt
 
 
 @cachier(stale_after=datetime.timedelta(days=3))
@@ -72,6 +104,6 @@ def get_ancestors(cls):
 
             
 if __name__ == "__main__":
-    validate()
+    validate_files()
 
 
---
+++ python/tests/test_against_examples.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+from types import ModuleType
+from typing import List
+from gocam_validator import validate
+import logging
+
+from rdflib import Graph
+
+SKIP_LIST = ['fail_no_evidence_4.ttl', 'fail_enabled_by_3.ttl', 'fail_IRE1-mediated_6.ttl', 'fail_part_of_1.ttl']
+
+class ValidateAgainstExamplesTestCase(unittest.TestCase):
+    """
+    Run GO-CAM validation test suite.
+
+    Currently this is broken into two sets, with data files in two directories:
+
+     - expected passes
+     - expected fails
+    """
+
+    @staticmethod
+    def _test_file_iter(subdir : str):
+        test_files = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../test_ttl/go_cams/' + subdir ))
+        for root, subdirs, files in os.walk(test_files):
+            for f in files:
+                if f in SKIP_LIST:
+                    logging.info(f"Skipping {f} -- REMEMBER TO COME BACK TO THIS LATER")            
+                    continue
+                logging.info(f"Validating {f}")            
+                yield validate(root + "/" + f)
+    
+    def test_positive_examples(self):
+        """ Test positive examples succeed """
+        n = 0
+        for rpt in self._test_file_iter('should_pass'):
+            n += 1
+            if not rpt.all_successful:
+                for (inst, sc, reason) in rpt.fail_list:
+                    print(f"FAIL: {inst} {sc} REASON: {reason}")
+            self.assertTrue(rpt.all_successful)
+        print(f"Ran {n} positive examples")
+
+    def test_negative_examples(self):
+        """ Test negative examples fail """
+        n = 0
+        for rpt in self._test_file_iter('should_fail'):
+            n += 1
+            if rpt.all_successful:
+                print(f"Expected at least one of the following to FAIL in {rpt.rdf_file}")
+                for (inst, sc, reason) in rpt.pass_list:
+                    print(f"  PASS [unexpected]: {inst} {sc}")
+            self.assertFalse(rpt.all_successful)
+        print(f"Ran {n} negative examples")
+
+
+if __name__ == '__main__':
+    unittest.main()
