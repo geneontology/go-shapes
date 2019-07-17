b9baffbde836f4f570d6e8e6ee804dd8e5c44c35
cmungall <cjm@berkeleybop.org>
2019-07-16 19:54:20 -0700

    Additional tests and framework, #38

--- util/cvt-ttlite.pl
+++ util/cvt-ttlite.pl
@@ -1,55 +1,101 @@
 #!/usr/bin/perl
 use strict;
 
-my %pmap = ();
-my $id = 0;
-my @lines = ();
 my $dir = './';
-if (@ARGV) {
-    $dir = $ARGV[0];
+
+my @lines;
+
+my %xmap;
+
+
+foreach my $fn (@ARGV) {
+    $dir = $fn;
     $dir =~ s@/[^/]*$@@;
     $dir .= '/';
-}
 
-print "## ========================================\n";
-print "## Auto-generated from ttlite file\n";
-print "## ========================================\n";
-print "## ** do not edit the file, edit the source **\n";
+    my %pmap = ();
+    my $id = 0;
 
-
-while(<>) {
-    chomp;
-    if (m@^include (.*)@) {
-        open(F,"$dir$1") || die $1;
-        while (<F>) {
-            chomp;
-            push(@lines, $_);
-        }
-        close(F);
-        next;
-    }
-    if (m/\@prefix (\S+): (\S+):(\S+) ! .*/i) {
-        $_ = "\@prefix $1: <http://purl.obolibrary.org/obo/$2_$3> .";
-    }
-    if (m@^(\S+): a (.*)@) {
-        $_ = "$1: a owl:NamedIndividual, $2";
-    }
-
-    if (m@^(\S+\d+):@) {
-        my $n = $1;
-        if (!$pmap{$n}) {
-            $id++;
-            $pmap{$n} = "http://model.geneontology.org/$id";
+    @lines = ();
+    %xmap = ();
+    push(@lines, "## ========================================");
+    push(@lines, "## Auto-generated from ttlite file");
+    push(@lines, "## ========================================");
+    push(@lines, "## ** do not edit the file, edit the source **");
+    open(F,$fn) || die $fn;
+    while(<F>) {
+        chomp;
+        if (m@^\#FAIL\s+(\S+)@) {
+            my $x = $1;
+            $xmap{$x} = [@lines, '# DELIBERATE ERROR BELOW:'];
+            while(<F>) {
+                if (m@^\#END@) {
+                    push(@{$xmap{$x}}, '# END OF DELIBERATE ERROR');
+                    last;
+                }
+                chomp;
+                s@^\#\s*@@;
+                push(@{$xmap{$x}}, $_);
+            }
+            next;
+            
         }
         
+        if (m@^include (.*)@) {
+            open(F2,"$dir$1") || die $1;
+            while (<F2>) {
+                chomp;
+                push(@lines, $_);
+            }
+            close(F2);
+            next;
+        }
+        if (m/\@prefix (\S+): (\S+):(\S+) ! .*/i) {
+            $_ = "\@prefix $1: <http://purl.obolibrary.org/obo/$2_$3> .";
+        }
+        if (m@^(\S+): a (.*)@) {
+            $_ = "$1: a owl:NamedIndividual, $2";
+        }
+
+        if (m@^(\S+\d+):@) {
+            my $n = $1;
+            if (!$pmap{$n}) {
+                $id++;
+                $pmap{$n} = "http://model.geneontology.org/$id";
+            }
+        }
+        addline($_);
     }
-    
-    push(@lines, $_);
+    close(F);
+    foreach my $p (keys %pmap) {
+        my $pline = "\@prefix $p: <$pmap{$p}> .";
+        unshift(@lines, $pline);
+        foreach my $x (keys %xmap) {
+            unshift(@{$xmap{$x}}, $_);        
+        }
+
+    }
+    foreach (@lines) {
+        print "$_\n";
+    }
+    foreach my $x (keys %xmap) {
+        my $ffn = $fn;
+        $ffn =~ s@p-(.*)\.ttlite@f-$1-$x.ttlite@;
+        open(OF, ">$ffn") || die $ffn;
+        foreach (@{$xmap{$x}}) {
+            print OF "$_\n";
+        }
+        close(OF);
+    }
 }
 
-foreach my $p (keys %pmap) {
-    print "\@prefix $p: <$pmap{$p}> .\n";
-}
-foreach (@lines) {
-    print "$_\n";
+
+exit 0;
+
+sub addline {
+    $_ = shift;
+    push(@lines, $_);
+    foreach my $x (keys %xmap) {
+        push(@{$xmap{$x}}, $_);        
+    }
 }
