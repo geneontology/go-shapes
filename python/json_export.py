ed26957ae185abfe65736a632faaf48152c8ca8a
tmushayahama <tmushayahama@gmail.com>
2022-09-28 22:44:45 -0700

    Cleaned the json a lil bit. Added lookup table dump

--- python/json_export.py
+++ python/json_export.py
@@ -17,7 +17,10 @@ class NoctuaFormShex:
         shex_response = requests.get(shex_url)
         self.shex = generate_shexj.parse(shex_response.text)
         pref = PrefixLibrary(shex_response.text)
-        self.pref_dict = {k:self.get_suffix(str(v)) for (k,v) in dict(pref).items()}
+        self.pref_dict = {
+            k:self.get_suffix(str(v)) for (k,v) in dict(pref).items() 
+             if str(v).startswith('http://purl.obolibrary.org/obo/')}
+        del self.pref_dict['OBO']
 
     def get_suffix(self, uri):
         suffix = contract_uri(uri, cmaps=[prefix_context])
@@ -27,27 +30,30 @@ class NoctuaFormShex:
         return path.basename(uri)
         
 
-    def get_shape_name(self, uri):
+    def get_shape_name(self, uri, clean=False):
         name = path.basename(uri).upper() 
         if '/go/' in uri:
             name = 'GO'+name
-        return self.pref_dict.get(name, uri)
+        return self.pref_dict.get(name, None if clean else uri )
+
+    def lookup_table(self):
+        table = {v: {
+                    'label':k
+                } for (k,v) in self.pref_dict.items()}
+        return table
+
 
     def _load_expr(self, expr: Optional[Union[shapeExprLabel, shapeExpr]], preds=None) -> List:
    
         if(preds == None):
             preds = {}
-
         if isinstance(expr, str) and isinstance(preds, list):
             preds.append(self.get_shape_name(expr))
-
         if isinstance(expr, (ShapeOr, ShapeAnd)):
             for expr2 in expr.shapeExprs:
                 self._load_expr(expr2, preds)
-
         elif isinstance(expr, ShapeNot):
             self._load_expr(expr.shapeExpr, preds)
-
         elif isinstance(expr, Shape):
             if expr.expression is not None:
                 self._load_triple_expr(expr.expression, preds)
@@ -60,19 +66,31 @@ class NoctuaFormShex:
         if isinstance(expr, (OneOf, EachOf)):
             for expr2 in expr.expressions:
                 self._load_triple_expr(expr2, preds)
-
         elif isinstance(expr, TripleConstraint):
             if expr.valueExpr is not None:
                 pred = self.get_suffix(expr.predicate)
-                preds[pred] = []
-                self._load_expr(expr.valueExpr, preds[pred])
+                
+                if pred not in self.pref_dict.values():
+                    return
+                
+                preds[pred] = {}
+                preds[pred]['targets'] = []
+                
+                if expr.max != None:
+                    preds[pred]['cardinality'] = expr.max                    
+                
+                self._load_expr(expr.valueExpr, preds[pred]['targets'])
 
     
     def parse(self):
         shapes = self.shex.shapes
 
         for shape in shapes:
-            shape_name = self.get_shape_name(shape['id'])          
+            shape_name = self.get_shape_name(shape['id'], True)
+    
+            if shape_name == None:
+                continue
+         
             print('Parsing Shape: ' + shape['id'])
             self.json_shapes[shape_name] = {}
 
@@ -80,13 +98,13 @@ class NoctuaFormShex:
 
             for expr in shexps:
                 self.json_shapes[shape_name] = self._load_expr(expr)
-            
-                   
+                               
      
-shelper = NoctuaFormShex()
-shelper.parse()
-
-
+nfShex = NoctuaFormShex()
+nfShex.parse()
 
 with open("shex_dump.json", "w") as sf:
-    json.dump(shelper.json_shapes, sf, indent=2)
+    json.dump(nfShex.json_shapes, sf, indent=2)
+
+with open("look_table.json", "w") as sf:
+    json.dump(nfShex.lookup_table(), sf, indent=2)
