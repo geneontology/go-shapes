b660d5022d903dc6b4ab96bddafd3de9cb394194
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-10-24 15:25:50 -0700

    fixing tests

--- python/tests/test_json_export.py
+++ python/tests/test_json_export.py
@@ -2,12 +2,10 @@ import pytest
 from linkml_runtime.utils.schemaview import SchemaView
 
 REMOTE_PATH = (
-    "https://raw.githubusercontent.com/geneontology/go-shapes/6b14a45b9f6e6b9dcb731d4201443b29ec5868e7/schema/shex_linkml.yaml"
+    "../../schema/shex_linkml.yaml"
 )
 
 
 def test_valid_schema():
-    sv = SchemaView(
-        "https://raw.githubusercontent.com/geneontology/go-shapes/6b14a45b9f6e6b9dcb731d4201443b29ec5868e7/schema/shex_linkml.yaml"
-    )
+    sv = SchemaView(REMOTE_PATH)
     assert sv.all_classes()
